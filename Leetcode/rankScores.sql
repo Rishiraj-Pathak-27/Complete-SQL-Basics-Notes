47afb7f186c3ec3a47c38de1383eb4967b2ced94
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-22 22:16:48 +0530

    questions added

--- Leetcode/rankScores.sql
+++ Leetcode/rankScores.sql
@@ -1,7 +1,6 @@
 -- 178. Rank Scores
 
 -- Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
-
 -- The scores should be ranked from the highest to the lowest.
 -- If there is a tie between two scores, both should have the same ranking.
 -- After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
@@ -12,3 +11,4 @@ SELECT s.score AS score,
             ORDER BY s.score DESC
        ) AS 'rank'
 FROM Scores s;
+
