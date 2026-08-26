ff80c5277604bc788cb3ac5b5a632971034fc576
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-26 22:27:10 +0530

    updates with minor changes

--- Leetcode/replaceEmpIdWithUniqueIdentifier.sql
+++ Leetcode/replaceEmpIdWithUniqueIdentifier.sql
@@ -3,8 +3,8 @@
 -- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 -- Return the result table in any order.
 
-SELECT e1.unique_id AS unique_id, e2.name AS name
-FROM EmployeeUNI e1
-LEFT JOIN Employees e2
+SELECT e2.unique_id AS unique_id, e1.name AS name
+FROM Employees e1
+LEFT JOIN EmployeeUNI e2
 ON e1.id=e2.id;
 
