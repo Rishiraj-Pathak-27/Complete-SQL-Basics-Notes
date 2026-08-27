5b0cbd74e74eebf68e9039aea1beb81ee6d8545f
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-27 22:09:53 +0530

    -- 197. Rising Temperature added

---
+++ Leetcode/risingTemperature.sql
@@ -0,0 +1,10 @@
+-- 197. Rising Temperature
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+-- Return the result table in any order.
+
+SELECT w1.id AS id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate)=1
+WHERE w1.temperature > w2.temperature;
